c8759f6de92558cf60f7a69449fc4abb48230e18
Miroslav Tsarev <mtzarev@gmail.com>
2024-11-05 23:30:12 +0200

    Update Replace Repeating Chars.js

--- Replace Repeating Chars.js
+++ Replace Repeating Chars.js
@@ -1,19 +1,11 @@
 function replaceRepeatingChars(input) {
-    let newArr = []
+    let newArr = [];
 for (let i=0; i<input.length; i++){
     if (input[i]!==input[i+1]){
-        newArr.push(input[i])
+        newArr.push(input[i]);
     }
 }
 console.log(newArr.join(``));
-// let newString =Array.from(input);
-// for (let char of newString){
-//     if (!newArr.includes(char)){
-//         newArr.push(char)
-//     }
-// }
-
-
 
 }
 replaceRepeatingChars('qqqwerqwecccwd');
